c2065ad5a68bd110d85efef855946f9111efe745
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-07 08:47:35 +0100

    add and use product card

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import AddToCard from "../AddToCard";
+
+const ProductCard = () => {
+	return (
+		<div>
+			ProductCard
+			<AddToCard />
+		</div>
+	);
+};
+
+export default ProductCard;
