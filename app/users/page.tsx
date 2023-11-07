2ca14c865f9e424f6d2f4d0464aca75ce7eb2870
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-07 08:46:44 +0100

    add pages

---
+++ app/users/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React, { Suspense } from "react";
+
+import UserTable from "./UserTable";
+
+interface Props {
+	searchParams: { sortOrder: string };
+}
+
+const UsersPage = ({ searchParams: { sortOrder } }: Props) => {
+	console.log(sortOrder);
+	return (
+		<>
+			<div>UsersPage</div>
+			<h1>Users</h1>
+			<Link href="/users/new" className="btn">
+				New
+			</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<UserTable sortOrder={sortOrder} />
+			</Suspense>
+		</>
+	);
+};
+
+export default UsersPage;
