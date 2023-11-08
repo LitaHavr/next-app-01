dd571e0f637ae3c4afc3e373cc717f3ab97a6763
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-08 12:06:07 +0100

    create route for emails

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,13 @@
+import {Resend} from "resend"
+
+import WelcomeTemplate from "@/emails/WelcomeTemplate"
+const resend = new Resend (process.env.RESEND_API_KEY)
+
+export async function POST() {
+	await resend.emails.send({
+		from:"feedback-smtp.us-east-1.amazonses.com",
+		to:"lolita.gavr@gmail.com",
+		subject:"....",
+		react: WelcomeTemplate ({name:"lita"})
+	})
+}
