774a51d9c41653ea627d57223d4db452ddc530f4
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-08 13:20:33 +0100

    create page for uploading

---
+++ app/upload/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { CldImage,CldUploadWidget } from 'next-cloudinary'
+import React,{useState} from 'react'
+interface CloudinaryResult{
+	public_id:string
+}
+
+const UploadPage = () => {
+	const [publicId,setPublicId] = useState("")
+	return (
+		<div>
+			{publicId && <CldImage src={publicId} width={270} height={180} alt="marvel"/>}
+			<CldUploadWidget
+			options={{sources:["camera","local","google_drive"],multiple:false,maxFiles:5}}
+			 uploadPreset='xlx3s4ip' onUpload={(result,widget)=>{if(result.event!=="success")return
+			const info = result.info as CloudinaryResult
+		 setPublicId(info.public_id)
+		}}>
+				{({open})=><button className='btn btn-primary' onClick={()=>open()}>Upload</button>}
+			</CldUploadWidget>
+		</div>
+	)
+}
+
+export default UploadPage
