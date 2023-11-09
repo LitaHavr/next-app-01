bfd390b34f808ba4dd3fdd0c6fb1be6c234a184b
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-09 11:25:43 +0100

    dynamic importexample

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
-import dynamic from "next/dynamic"
-import { useState } from "react";
-
-
-const HeavyComponent = dynamic(()=>import ("./components/HeavyComponent"),{ssr:false,loading:()=><p>Loading...</p>})
-
 export default  function Home() {
-const [isVisible,setVisible]=useState(false)
 	return (
 		<main className="relative h-screen">
 			<h1 className="font-poppins">Hello </h1>
-			<button onClick={()=>setVisible(true)}>Show</button>
-			{isVisible &&<HeavyComponent/> }
+			<button onClick={async()=>{
+				const _ = (await import ("lodash")).default
+				const users =[
+					{name:"c"},
+					{name:"b"},
+					{name:"d"}
+				]
+				const sortedUsers=_.orderBy(users,["name"])
+			}}>Show</button>
       
 		</main>
 	);
