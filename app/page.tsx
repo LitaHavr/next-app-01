1fff41887229ba58ac6da553547b89e8511b7115
Lolita Havrychko <lolita.havrychko@synetech.cz>
2023-11-09 08:37:01 +0100

    use next image

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import Link from "next/link";
+import Image from "next/image"
 import { getServerSession } from "next-auth";
 
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import ProductCard from "./components/ProductCard/ProductCard";
 export default async function Home() {
 
 	const session = await getServerSession(authOptions)
 	return (
-		<main>
-			<h1>hello {session && <span>{session.user!.name}</span>} </h1>
-			<Link href="/users">Users</Link>
-			<ProductCard />
+		<main className="relative h-screen">
+	<Image 
+	 src="https://bit.ly/react-cover"
+	 alt="coffee"
+	fill
+	className="object-cover"
+	sizes="(max-width:480px) 100vw,(max-width:768px) 50vw, 33vw"
+	/>
 		</main>
 	);
 }
